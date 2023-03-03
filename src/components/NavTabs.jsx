cd3a6a6c28dcb8f3202fc6c052bbc734305f4345
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-03-03 21:17:36 +0000

    added a the ability to hide the menu items when the viewport is smallest

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,91 +1,82 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import { useState } from "react";
 
 /*MUI App Bar imports*/
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
-const drawerWidth = 240;
-const navItems = ["Home", "About", "Contact"];
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
 
 function NavTabs(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+  //This uses UseState hook which returns an array with the variable menuOpen which is false and the function setMenuOpen which updates the useState variable, as the state is false the burger menu is closed. The function when called will update the menuOpen state to true (open) or false (closed)//
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  console.log(useState(false));
+
+  //When this function is called, it sets the variable menuOpen state to true(open)//
+  const handleMenuOpen = () => {
+    setMenuOpen(true);
   };
 
-  // const drawer = (
-  //   <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-  //     <Typography variant="h6" sx={{ my: 2 }}>
-  //       MUI
-  //     </Typography>
-  //     <Divider />
-  //     <List>
-  //       {navItems.map((item) => (
-  //         <ListItem key={item} disablePadding>
-  //           <ListItemButton sx={{ textAlign: "center" }}>
-  //             <ListItemText primary={item} />
-  //           </ListItemButton>
-  //         </ListItem>
-  //       ))}
-  //     </List>
-  //   </Box>
-  // );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+  //When this function is called, it sets the variable menuOpen state to false(closed)//
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <>
       <AppBar component="nav">
         <Toolbar sx={{ backgroundColor: "#343434" }}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
+        <Typography
             variant="h6"
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
             VINH_LA
           </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleMenuOpen}//Opens the burger menu//
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+   
 
           <Box
             component="nav"
-            sx={{ display: "flex", justifyContent: "space-between", backgroundColor: "#343434!important" }}
+            sx={{ display: "flex", justifyContent: "space-between", backgroundColor: "#343434" }}
           >
             <ButtonGroup
               variant="contained"
               aria-label="outlined primary button group"
-              sx={{backgroundColor: "#343434!important"}}
+              sx={{
+                backgroundColor: "#343434",
+                display: { xs: "none", sm: "flex" }, // hide on mobile but show on desktop when the viewport is smallest to hide the other menu options//
+              }}
             >
               <Button
                 component={NavLink}
                 to="/reactPortfolio/"
                 end
                 activeClassName="active"
-                sx={{ p: 2, color: "#fff",backgroundColor: "#343434!important", borderColor: "#343434!important"}}
+                sx={{ p: 2, color: "#fff", backgroundColor: "#343434", borderColor: "#343434!important"}}
               >
                 Home
               </Button>
@@ -94,7 +85,7 @@ function NavTabs(props) {
                 to="/reactPortfolio/"
                 end
                 activeClassName="active"
-                sx={{ p: 2, color: "#fff", backgroundColor: "#343434!important", borderColor: "#343434!important" }}
+                sx={{ p: 2, color: "#fff", backgroundColor: "#343434", borderColor: "#343434!important" }}
               >
                 Project
               </Button>
@@ -103,7 +94,7 @@ function NavTabs(props) {
                 to="/reactPortfolio/"
                 end
                 activeClassName="active"
-                sx={{ p: 2, color: "#fff", backgroundColor: "#343434!important" , borderColor: "#343434!important"}}
+                sx={{ p: 2, color: "#fff", backgroundColor: "#343434", borderColor: "#343434!important" }}
               >
                 Blog
               </Button>
@@ -112,58 +103,26 @@ function NavTabs(props) {
                 to="/reactPortfolio/"
                 end
                 activeClassName="active"
-                sx={{ p: 2, color: "#fff", backgroundColor: "#343434!important", borderColor: "#343434!important" }}
+                sx={{ p: 2, color: "#fff", backgroundColor: "#343434", borderColor: "#343434" }}
               >
                 Contact
               </Button>
             </ButtonGroup>
           </Box>
+        
+          <Menu
+         anchorEl={menuOpen}
+         open={menuOpen}
+         onClose={handleMenuClose}
+         //When a menu item is clicked the burger menu will close//
+          >
+            <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Project</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Blog</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
-
-      {/* <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <NavLink
-          to="/reactPortfolio/"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Home
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/reactPortfolio/project"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          project
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/reactPortfolio/blog"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Blog
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/reactPortfolio/contact"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Contact
-        </NavLink>
-      </li>
-    </ul> */}
     </>
   );
 }
